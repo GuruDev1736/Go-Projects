603adff49b8df8044e008784ea77f7f863ef12ce
Guruprasad <guruprasad1736@gmail.com>
2023-11-19 15:20:29 +0530

    cooments done

--- Golang Mysql BookStore Management/pkg/controllers/book-controller.go
+++ Golang Mysql BookStore Management/pkg/controllers/book-controller.go
@@ -38,45 +38,45 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreateBook(w http.ResponseWriter, r *http.Request) {
-	CreateBook := &models.Book{}
+	CreateBook := &models.Book{} // geting the book reference from the model folder
 	if err := utils.ParseBody(r, CreateBook); err != nil {
 		// Handle the error, e.g., return a bad request response
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	b := CreateBook.CreateBook()
-	res, _ := json.Marshal(b)
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
+	b := CreateBook.CreateBook() // after getting the instance of the Book struct then we call the create book function and stored the funtion inot the 'b'
+	res, _ := json.Marshal(b)    // decode the data into the json
+	w.WriteHeader(http.StatusOK) // send the status ok
+	w.Write(res)                 // show the response
 }
 
 func DeleteBook(w http.ResponseWriter, r *http.Request) {
 
-	vars := mux.Vars(r)
+	vars := mux.Vars(r) // create the variable vars to store the database variables / instance
 
-	bookId := vars["bookId"]
+	bookId := vars["bookId"] // search the variable bookId in the database and stored in the bookId
 
-	ID, err := strconv.ParseInt(bookId, 0, 0)
+	ID, err := strconv.ParseInt(bookId, 0, 0) // convert the bookId into integer
 	if err != nil {
-		fmt.Println("Error While Parsing")
+		fmt.Println("Error While Parsing") // catch the error while parsing
 	}
 
-	book := models.DeleteBook(ID)
+	book := models.DeleteBook(ID) // calling the function DeleteBook form the model package and give the parameter id ans store the result into the book variable
 
-	res, err := json.Marshal(book)
+	res, err := json.Marshal(book) // decode the data and store into the res
 	if err != nil {
-		fmt.Println("There is the error to marshal")
+		fmt.Println("There is the error to marshal") // handle the error
 	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
+	w.Header().Set("Content-Type", "application/json") // showing the response into the json
+	w.WriteHeader(http.StatusOK)                       // ok status
+	w.Write(res)                                       // shoiwing the response
 }
 
 func UpdateBook(w http.ResponseWriter, r *http.Request) {
-	var updateBook = &models.Book{}
+	var updateBook = &models.Book{} // get the reference of the Book from the model package
 
-	utils.ParseBody(r, updateBook)
+	utils.ParseBody(r, updateBook) // use the parsebody function form the utils package and give the parameters to it
 	vars := mux.Vars(r)
 	bookId := vars["bookId"]
 	ID, err := strconv.ParseInt(bookId, 0, 0)
